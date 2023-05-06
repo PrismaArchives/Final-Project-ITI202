be375d53a730871911ac178e5a3e7582f159ea22
Prisma <34573574+PrismaArchives@users.noreply.github.com>
2023-05-06 19:00:45 -0400

    Update final.py
    Started compare function for comparing data from the files generated by each year.

--- final.py
+++ final.py
@@ -28,7 +28,6 @@ def get_article_count():
     response = requests.get(url, params)
     content = response.json()
     hits = content['response']['meta']['hits']
-    print(hits)
     return hits
 
 
@@ -43,13 +42,22 @@ def search_terms_in_year(year, term_list: list):
     for index in term_list:
         set_params(f"pub_year: {year}", index)
         results_dict[year][index] = get_article_count()
-    print(results_dict)
-    
+    json_formatted = json.dumps(results_dict, indent=4)
+    #print(json_formatted)
+    with open(f"{year}_hits.json", "w") as file:
+        file.write(json_formatted)
 
+
+#will take created json files and compare the hits for each term 
 def compare_years(first_year, second_year, term_list):
-    
-    pass
+    with open(f'{first_year}_hits.json') as openfile:
+        first_year_json = json.load(openfile)
+    with open(f'{second_year}_hits.json') as openfile:
+        second_year_json = json.load(openfile)
+    print(first_year_json)
+    print(second_year_json)
 
 search_terms_in_year(2023,search_term_list)
 params["api-key"] = SECOND_API_KEY
 search_terms_in_year(2013,search_term_list)
+compare_years(2013,2023,search_term_list)
